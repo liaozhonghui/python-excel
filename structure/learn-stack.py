1d9b2777c910434f1907d03f9cf949e8b20d9501
廖忠辉 <liaozhonghui@playdayy.com>
2022-05-05 20:30:28 +0800

    feat: modify import scripts

---
+++ structure/learn-stack.py
@@ -0,0 +1,25 @@
+# stack()
+# push(item)
+# pop()
+# peek()
+# isEmpty()
+# size()
+
+class Stack:
+    def __init(self):
+        self.items = []
+    
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self,item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items) - 1]
+
+    def size(self): 
+        return len(self.items)
